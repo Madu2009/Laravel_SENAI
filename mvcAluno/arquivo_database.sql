1a6e4b5f039184eff8801b2acfcf6f2e6dc1257d
Maria Eduarda <maria.e.evangelista7@aluno.senai.br>
2026-03-23 08:00:14 -0300

    primeiro commit

---
+++ mvcAluno/arquivo_database.sql
@@ -0,0 +1,10 @@
+create database alunoLaravel;
+use alunoLaravel
+
+CREATE TABLE alunos(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100),
+email VARCHAR(100),
+created_at timestamp null,
+updated_at timestamp null
+);
